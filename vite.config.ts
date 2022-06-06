3538ca318adbbdd87af7ea81d17c7e9a862fd3fd
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-06-06 18:02:05 +0900

    chore: aliasを設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import path from 'path'
+import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [solidPlugin()],
   build: {
     target: 'esnext',
-    polyfillDynamicImport: false,
+    polyfillDynamicImport: false
   },
-});
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, 'src/components'),
+      types: path.resolve(__dirname, 'src/types')
+    }
+  }
+})
