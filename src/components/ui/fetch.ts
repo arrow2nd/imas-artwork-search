c48c95928a118451a752f778c7ebcfec0ce2732a
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-06-07 11:21:06 +0900

    feat: APIのアクセスエラー処理を実装

--- src/components/ui/fetch.ts
+++ src/components/ui/fetch.ts
@@ -17,23 +17,46 @@ export const fetchArtworks = async ({
   url.searchParams.append('orderby', 'asc')
   url.searchParams.append('limit', '25')
 
-  const data = await fetch(url.href)
+  const data = await fetchFromApi(url.href)
   const json = await data.json()
 
   if (!data.ok) {
-    throw new Error(json.message || data.statusText)
+    throw createNewError(json, data)
   }
 
   return json
 }
 
 const feachFromCdId = async (keyword: string): Promise<any[]> => {
-  const data = await fetch(`https://imas-artwork-api.deno.dev/v1/cd/${keyword}`)
+  const data = await fetchFromApi(
+    `https://imas-artwork-api.deno.dev/v1/cd/${keyword}`
+  )
   const json = await data.json()
 
   if (!data.ok) {
-    throw new Error(json.message || data.statusText)
+    throw createNewError(json, data)
   }
 
   return [json]
+}
+
+const fetchFromApi = async (url: string): Promise<Response> => {
+  try {
+    // 5秒でタイムアウトさせる
+    const ctrl = new AbortController()
+    const id = setTimeout(() => ctrl.abort(), 5000)
+
+    const data = await fetch(url)
+    clearTimeout(id)
+
+    return data
+  } catch (err) {
+    console.error(err)
+    throw new Error('Failed to access API')
+  }
+}
+
+const createNewError = (json: any, data: Response): Error => {
+  const message = json.message || data.statusText
+  return new Error(`${message} ( ${data.status} )`)
 }
